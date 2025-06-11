1925f0a4f1e1db398a308e7106c09bcbfdd900cc
Miles Porter <portermiles91@gmail.com>
2025-06-11 13:42:30 -0600

    stub out database, test stack

--- Fuel4LessJPA/src/main/java/com/skilldistillery/fuel4less/entities/User.java
+++ Fuel4LessJPA/src/main/java/com/skilldistillery/fuel4less/entities/User.java
@@ -1,5 +1,108 @@
 package com.skilldistillery.fuel4less.entities;
 
-public class User {
+import java.util.Objects;
 
+import jakarta.persistence.Entity;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
+
+@Entity
+public class User {
+	// Fields
+	@Id
+	@GeneratedValue(strategy = GenerationType.IDENTITY)
+	private int id;
+	private String username;
+	private String password;
+	private String email;
+	private boolean enabled;
+	private String role;
+	
+	// Constructors
+	public User() {
+		super();
+	}
+
+	// Getters & Setters
+	public int getId() {
+		return id;
+	}
+
+
+	public void setId(int id) {
+		this.id = id;
+	}
+
+
+	public String getUsername() {
+		return username;
+	}
+
+
+	public void setUsername(String username) {
+		this.username = username;
+	}
+
+
+	public String getPassword() {
+		return password;
+	}
+
+
+	public void setPassword(String password) {
+		this.password = password;
+	}
+
+
+	public String getEmail() {
+		return email;
+	}
+
+
+	public void setEmail(String email) {
+		this.email = email;
+	}
+
+
+	public boolean isEnabled() {
+		return enabled;
+	}
+
+
+	public void setEnabled(boolean enabled) {
+		this.enabled = enabled;
+	}
+
+
+	public String getRole() {
+		return role;
+	}
+
+
+	public void setRole(String role) {
+		this.role = role;
+	}
+
+	//hashCode and equals
+	@Override
+	public int hashCode() {
+		return Objects.hash(id);
+	}
+
+
+	@Override
+	public boolean equals(Object obj) {
+		if (this == obj)
+			return true;
+		if (obj == null)
+			return false;
+		if (getClass() != obj.getClass())
+			return false;
+		User other = (User) obj;
+		return id == other.id;
+	}
+	
+	
+	
 }
--- Fuel4LessJPA/src/test/java/com/skilldistillery/fuel4less/entities/UserTest.java
+++ Fuel4LessJPA/src/test/java/com/skilldistillery/fuel4less/entities/UserTest.java
@@ -1,14 +1,56 @@
 package com.skilldistillery.fuel4less.entities;
 
-import static org.junit.jupiter.api.Assertions.*;
+import static org.junit.jupiter.api.Assertions.assertEquals;
+import static org.junit.jupiter.api.Assertions.assertNotNull;
+import static org.junit.jupiter.api.Assertions.assertTrue;
 
+import org.junit.jupiter.api.AfterAll;
+import org.junit.jupiter.api.AfterEach;
+import org.junit.jupiter.api.BeforeAll;
+import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
 
-class UserTest {
+import jakarta.persistence.EntityManager;
+import jakarta.persistence.EntityManagerFactory;
+import jakarta.persistence.Persistence;
 
+class UserTest {
+	
+	private static EntityManagerFactory emf;
+	private static EntityManager em;
+	private User user;
+	
+	
+	@BeforeAll
+	 static void setUpBeforeClass() throws Exception {
+		emf = Persistence.createEntityManagerFactory("Fuel4LessJPA");
+	 }
+
+
+	 @AfterAll
+	 static void tearDownAfterClass() throws Exception {
+		 emf.close();
+	 }
+
+
+	 @BeforeEach
+	 void setUp() throws Exception {
+		 em = emf.createEntityManager();
+		 user = em.find(User.class, 1);
+	 }
+
+
+	 @AfterEach
+	 void tearDown() throws Exception {
+		 em.close();
+		 user = null;
+	 }
 	@Test
-	void test() {
-		fail("Not yet implemented");
+	void test_User_mapping() {
+		assertNotNull(user);
+		assertEquals("test", user.getUsername());
+		assertEquals("admin", user.getRole());
+		assertTrue(user.isEnabled());
 	}
 
 }
